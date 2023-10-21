7927fcc9efe11e2f23141464ec499adfad030598
Ginger Owen <gingerowen0421@gmail.com>
2023-10-21 10:21:58 -0600

    worked on dbservice

--- server/DbService.js
+++ server/DbService.js
@@ -48,7 +48,7 @@ class DbService {
     async createNew(name) { //changed from insertNewName
         try { 
             const response = await new Promise((resolve,reject) => {
-                const query = "INSERT INTO fullstack_db (name, date_added) VALUES ? " //stopped 43
+                const query = "INSERT INTO fullstack_db (name, date_added) VALUES (?, ?);"; //stopped 43
 
                 connection.query(query, (error, results) => {
                     if (error) reject (new Error(error.message));
