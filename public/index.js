e1ed382ca1445e7b3c91e9caefb0c4e4052595e6
Ginger Owen <gingerowen0421@gmail.com>
2023-10-26 08:55:49 -0600

    index.js worked on inserting data into table funct

--- public/index.js
+++ public/index.js
@@ -36,13 +36,20 @@ function renderHTMLTable(data) {
 
     console.log(data);
     //commented out the line below
-    //let tableHTML = " ";
+    let tableHTML = " ";
 
     if (data.length === 0) {
         table.innerHTML="<tr><td class='no-data' colspan='5' >Table is empty</tr></td>"
+        return; 
     }
 
-    data.forEach(function ({ID, name, dtae_added}) => {
-        
+    data.forEach(function ({ID, name, date_added}) {
+        tableHTML += "<tr>";
+        tableHTML += `<td>${ID}</td>`;
+        tableHTML += `<td>${name}</td>`;
+        tableHTML += `<td>${date_added}</td>`;
+        tableHTML += `<td><button fdks>Delete</button></td>`;
+        tableHTML += `<td><button dsj> Edit</button></td>`
     });// end forEach
+    //STOPPED 48MINS
 } //end loadHTMLTable
