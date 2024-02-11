d72b6c475f3543aa961f61aeb75e663031456072
Ginger Owen <gingerowen0421@gmail.com>
2024-02-10 17:24:29 -0700

    update readme and fiddled with index.js but not working right

--- public/index.js
+++ public/index.js
@@ -11,45 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
      
 }); 
 
-//callbackfunction to grab data and add data to database
-addBtn.onclick = function() {
-    const nameData = document.getElementById('name');
-    const name = nameData.value;
-    nameData.value = "";
-
-    fetch('http://localhost:3001/addData', {
-    method:'POST',
-    body:JSON.stringify({ name : name }),
-    headers: {'Content-Type': 'application/json'}
-}) //end fetch
-    .then(response => response.json())
-    .then(data => insertRow(data['data']));
-    
-    //insertRow() //am working ahead nnklnklmklmk
-} //end addBtn.onClick
-
-function renderHTMLTable(data) {
-
-    console.log(data);
-
-    if (data.length === 0) {
-        table.innerHTML="<tr><td class='no-data' colspan='5' >Table is empty</tr></td>"
-        return; 
-    }
-
-    data.forEach(function ({ID, name, date_added}) {
-        tableHTML += "<tr>";
-        tableHTML += `<td>${ID}</td>`;
-        tableHTML += `<td>${name}</td>`;
-        tableHTML += `<td>${new Date(date_added).toLocaleString()}</td>`;
-        tableHTML += `<td><button class="delete-row" data-id=${ID}>Delete</button></td>`;
-        tableHTML += `<td><button class="edit" data-id=${ID}>Edit</button></td>`
-        tableHTML += "</tr>"
-    });// end forEach
-
-    table.innerHTML = tableHTML;
-} //end loadHTMLTable
-
 function insertRow (data) {
     console.log(data);
     //stopped 57 mins
@@ -81,6 +42,77 @@ function insertRow (data) {
     } //end if else
 
 } //end insertRow
+
+//callbackfunction to grab data and add data to database
+addBtn.onclick = function() {
+    const nameData = document.getElementById('name');
+    const name = nameData.value;
+    nameData.value = "";
+
+    fetch('http://localhost:3001/addData', {
+    method:'POST',
+    body:JSON.stringify({ name : name }),
+    headers: {'Content-Type': 'application/json'}
+}) //end fetch
+    .then(response => response.json())
+    .then(data => insertRow(data['data']));
+    
+    insertRow(data) 
+} //end addBtn.onClick
+
+function renderHTMLTable(data) {
+
+    console.log(data);
+
+    if (data.length === 0) {
+        table.innerHTML="<tr><td class='no-data' colspan='5' >Table is empty</tr></td>"
+        return; 
+    }
+
+    data.forEach(function ({ID, name, date_added}) {
+        tableHTML += "<tr>";
+        tableHTML += `<td>${ID}</td>`;
+        tableHTML += `<td>${name}</td>`;
+        tableHTML += `<td>${new Date(date_added).toLocaleString()}</td>`;
+        tableHTML += `<td><button class="delete-row" data-id=${ID}>Delete</button></td>`;
+        tableHTML += `<td><button class="edit" data-id=${ID}>Edit</button></td>`
+        tableHTML += "</tr>"
+    });// end forEach
+
+    table.innerHTML = tableHTML;
+} //end loadHTMLTable
+
+// function insertRow (data) {
+//     console.log(data);
+//     //stopped 57 mins
+//     //check to see if ...dsjk?????
+//     const hasData = table.querySelector("no-data");
+//     let tableHTML = "<tr>";
+
+//     for (var key in data) {
+//         if (data.hasOwnProperty(key)) {
+//             if (key === dateAdded) {
+//                 data [key] = new Date (data[key]).toLocaleString()
+//             }
+
+//             tableHTML += `<td>${data[key]}</td>`
+//         }
+        
+//     }
+
+//         tableHTML += `<td><button class="delete-row" data-id=${data.ID}>Delete</button></td>`;
+//         tableHTML += `<td><button class="edit" data-id=${data.ID}>Edit</button></td>`
+
+//     tableHTML += "</tr>";
+
+//     if (hasData) {
+//         table.innerHTML = tableHTML;
+//     } else {
+//         const newRow = table.insertRow();
+//         newRow.innerHTML = tableHTML;
+//     } //end if else
+
+// } //end insertRow
 
 const deleteBtn = document.getElementsByClassName("delete-row");
 
